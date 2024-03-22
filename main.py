e8b1dc73790aff2451eaa3d442a9ecb9647124f6
Aline Areda <aredaaline@gmail.com>
2024-03-22 00:30:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fila_prioritaria import FilaPrioritaria
+from fila_normal import FilaNormal
+
+fila_teste = FilaPrioritaria()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chama_cliente(5))
+print(fila_teste.chama_cliente(8))
+print(fila_teste.estatisticas('21/03/2024', 500, 'detail'))
