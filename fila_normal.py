551a02f5d2ec3b8d04c50356f7de658790b4e8d0
Aline Areda <aredaaline@gmail.com>
2024-03-22 00:45:56 -0300

    testing flake8 hook2

--- fila_normal.py
+++ fila_normal.py
@@ -1,23 +1,24 @@
 class FilaNormal:
-
-    codigo:int = 0
+    codigo: int = 0
     fila = []
-    clientesatendidos = []
-    senhaatual:str = ''
+    clientes_atendidos = []
+    senha_atual: str = ''
 
-    def gerasenhaatual(self)->None:
-        self.senhaatual = f'NM{self.codigo}'
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f'PR{self.codigo}'
 
-    def resetafila(self)->None:
+    def reseta_fila(self) -> None:
         if self.codigo >= 100:
-            self.codigo=0
+            self.codigo = 0
         else:
-            self.codigo+=1
-    def atualizafila(self)->None:
-        self.resetafila()
-        self.gerasenhaatual()
-        self.fila.append(self.senhaatual)
-    def chamacliente(self, caixa:int)->str:
-        cliente_atual:str = self.fila.pop(0)
-        self.clientesatendidos.append(cliente_atual)
-        return(f'Cliente atual: {cliente_atual} dirija-se ao caixa: {caixa}')
+            self.codigo += 1
+
+    def atualiza_fila(self) -> None:
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.fila.append(self.senha_atual)
+
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return (f'Cliente atual: {cliente_atual} dirija-se ao caixa: {caixa}')
