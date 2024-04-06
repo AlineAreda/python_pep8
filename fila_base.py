de5bcaaee76946f02521d5633f438fbbf3d6fa87
Aline Areda <aredaaline@gmail.com>
2024-04-05 21:30:30 -0300

    Subindo constantes

--- fila_base.py
+++ fila_base.py
@@ -1,11 +1,31 @@
-class FilaBase:
+import abc
+from constantes import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
+
+class FilaBase(metaclass=abc.ABCMeta):
     codigo: int = 0
     fila = []
     clientes_atendidos = []
     senha_atual: str = ''
 
     def reseta_fila(self) -> None:
-        if self.codigo >= 200:
-            self.codigo = 0
+        if self.codigo >= TAMANHO_PADRAO_MAXIMO:
+            self.codigo = TAMANHO_PADRAO_MINIMO
         else:
             self.codigo += 1
+
+    def inseri_cliente(self):
+        self.fila.append(self.senha_atual)
+
+    def atualiza_fila(self):
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.inseri_cliente()
+
+    # metodos abstratos
+    @abc.abstractmethod
+    def gera_senha_atual(self):
+        ...
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa: int):
+        ...
