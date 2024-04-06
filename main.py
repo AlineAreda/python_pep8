30537f0a46a8be8a778c23922e077790302f54d9
Aline Areda <aredaaline@gmail.com>
2024-04-06 01:35:17 -0300

    alterando typing

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from fila_prioritaria import FilaPrioritaria
-from fila_normal import FilaNormal
+from fabrica_fila import FabricaFila
 
-fila_teste = FilaPrioritaria()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
-print(fila_teste.chama_cliente(8))
-print(fila_teste.estatisticas('21/03/2024', 341, 'detail'))
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(8))
+# print(fila_teste.estatisticas('21/03/2024', 341, 'detail'))
